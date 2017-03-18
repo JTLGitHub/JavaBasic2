42ea0d9744906d253238c337874dfa7895f180ba
JTLGitHub <jitendra.t.lad@gmail.com>
2017-03-17 21:58:31 -0700

    Third commit

---
+++ JavaBasic2/src/pkg_basic/cls_basic2.java
@@ -0,0 +1,18 @@
+package pkg_basic;
+
+public class cls_basic2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+			System.out.println("Hi");
+			
+	}
+	public static void main1(String[] args) {
+		// TODO Auto-generated method stub
+
+			System.out.println("Hi");
+			
+	}
+
+}
